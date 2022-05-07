7fe87982b8d2ac7c1d451404be620a5dbab878ec
Teodor Videlov <91604846+TeodorVidelov@users.noreply.github.com>
2022-05-07 22:59:30 +0300

    Add files via upload

---
+++ Basic Syntax, Conditional Statements and Loops - Lab/src/BackIn30Minutes.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class BackIn30Minutes {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int hours = Integer.parseInt(scanner.nextLine());
+        int minutes = Integer.parseInt(scanner.nextLine());
+
+        minutes += 30;
+
+        while (minutes >= 60){
+             minutes -= 60;
+             hours++;
+        }
+        while (hours >= 24){
+            hours -= 24;
+        }
+        System.out.printf("%d:%02d",hours,minutes);
+    }
+}
