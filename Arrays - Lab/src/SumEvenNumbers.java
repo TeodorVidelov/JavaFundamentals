640f0e98dbc4fcc5d04bd582dddabdb5460dfcd5
Teodor Videlov <91604846+TeodorVidelov@users.noreply.github.com>
2022-06-14 22:57:42 +0300

    Add files via upload

---
+++ Arrays - Lab/src/SumEvenNumbers.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+import java.util.Scanner;
+public class SumEvenNumbers {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int[] numbers = Arrays.stream(scanner.nextLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+
+        int sumEven = 0;
+
+        for (int i = 0; i < numbers.length; i++) {
+
+            if (numbers[i] % 2 == 0){
+                sumEven += numbers[i];
+            }
+        }
+        System.out.printf("%d",sumEven);
+    }
+}
