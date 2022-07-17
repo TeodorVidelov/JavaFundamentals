d4a43df6e175e98914582e8fc7026f060fc580e8
Teodor Videlov <91604846+TeodorVidelov@users.noreply.github.com>
2022-07-17 22:56:07 +0300

    Add files via upload

---
+++ Arrays - Exercise/src/TopIntegers.java
@@ -0,0 +1,24 @@
+import java.util.Arrays;
+import java.util.Scanner;
+public class TopIntegers {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int[] array = Arrays.stream(scanner.nextLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+
+        for (int i = 0; i < array.length; i++) {
+            boolean isTop = false;
+
+            for (int j = i + 1; j < array.length; j++) {
+
+                if (array[i] <= array[j]) {
+                    isTop = true;
+
+                }
+            }
+            if (!isTop) {
+                    System.out.print(array[i] + " ");
+            }
+        }
+    }
+}
